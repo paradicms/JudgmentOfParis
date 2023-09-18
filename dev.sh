6f3048388ee0345c5ec78f85ee29d5a5cf571492
Minor Gordon <minorg@server.fake>
2023-09-18 18:33:35 -0400

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -11,16 +11,17 @@ PROJECTS_DIR_PATH=$PWD/..
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
-poetry run $PROJECTS_DIR_PATH/paradicms-spreadsheet-etl-action/action.py \
-  --cache-directory-path $CACHE_DIR_PATH \
-  --force-extract \
-  --loaded-data-directory-path $DATA_DIR_PATH \
-  --pipeline-id JudgmentOfParis \
-  --spreadsheet "1WdwolQU728-DQSnNduXPyVAZloutGKFkUBKTXIqHZcU" \
-  "$@"
+# poetry run $PROJECTS_DIR_PATH/paradicms-spreadsheet-etl-action/action.py \
+#   --cache-directory-path $CACHE_DIR_PATH \
+#   --force-extract \
+#   --loaded-data-directory-path $DATA_DIR_PATH \
+#   --pipeline-id JudgmentOfParis \
+#   --spreadsheet "1WdwolQU728-DQSnNduXPyVAZloutGKFkUBKTXIqHZcU" \
+#   "$@"
 
 poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
+  --client-api rest \
   --data-path $DATA_DIR_PATH \
-  --dev \
+  --next-commands dev \
   --pipeline-id JudgmentOfParis
